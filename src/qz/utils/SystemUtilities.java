8a0ecb2f1d30c529a2dd8298d9359eb05bf167ef
distmineiradebaterias <contato@distmineiradebaterias.com.br>
2025-05-27 00:18:33 -0300

    Update SystemUtilities.java

--- src/qz/utils/SystemUtilities.java
+++ src/qz/utils/SystemUtilities.java
@@ -801,8 +801,10 @@ public class SystemUtilities {
                     break;
                 case MAC:
                     break;
+                case LINUX:
+                    break;
                 default:
-                    // Linux System Tray support is abysmal, always use TaskbarTrayIcon
+                    // Others Linux System Tray support is abysmal, always use TaskbarTrayIcon
                     return false;
             }
             return SystemTray.isSupported();
